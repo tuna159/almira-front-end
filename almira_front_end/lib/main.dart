3d6ff548aa6a75691a82bccb50e6a6ffa2cf15c1
tunagch190847 <anhtungo159@gmail.com>
2023-01-10 09:44:07 +0700

    build-routes login and register

--- almira_front_end/lib/main.dart
+++ almira_front_end/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:almira_front_end/home/home-app.dart';
 import 'package:almira_front_end/routes/routes.dart';
+import 'package:almira_front_end/welcome-app/register.dart';
+import 'package:almira_front_end/welcome-app/sign-up.dart';
 import 'package:almira_front_end/welcome-app/welcome.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +16,8 @@ class ApplicationNetWork extends StatelessWidget {
       routes: {
         RouteNames.Welcome: (context) => const Welcome(),
         RouteNames.HomeApp: (context) => const HomeApp(),
+        RouteNames.SignUp: (context) => const SignUp(),
+        RouteNames.Register: (context) => const Register(),
       },
       initialRoute: RouteNames.Welcome,
       // home: Welcome(),
--- almira_front_end/lib/routes/routes.dart
+++ almira_front_end/lib/routes/routes.dart
@@ -3,4 +3,6 @@ import 'package:almira_front_end/welcome-app/welcome.dart';
 class RouteNames {
   static const HomeApp = "/home_app";
   static const Welcome = "/welcome";
+  static const SignUp = "/sign_up";
+  static const Register = "/register";
 }
